a119fbca35344b94e3889cb14d94af82f9b9d529
Janez Urevc <janez@janezurevc.name>
2025-10-25 12:15:19 +0200

    SHM gone, random generation in.

--- main.go
+++ main.go
@@ -1,49 +1,25 @@
-/*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
+// main.go - SHM to Venus OS DBus adapter with random data generation
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
-	"net"
+	"math/rand"
 	"os"
 	"os/signal"
-	"strconv"
 	"strings"
 	"sync"
 	"syscall"
+	"time"
 
-	"github.com/dmichael/go-multicast/multicast"
 	"github.com/godbus/dbus/introspect"
 	"github.com/godbus/dbus/v5"
 	log "github.com/sirupsen/logrus"
 )
 
-const (
-	address = "239.12.255.254:9522"
-)
-
-var isEnergyMeter bool // true if it is a 1.0 energy meter, false if it is a 2.0 SHM
-
 // Config holds all configuration for the application
 type Config struct {
-	MulticastAddress string
-	DBusName         string
-	SMASusyID        uint32
-	LogLevel         string
+	DBusName string
+	LogLevel string
 }
 
 // App represents the main application
@@ -53,27 +29,6 @@ type App struct {
 	values     map[int]map[objectpath]dbus.Variant
 	mu         sync.RWMutex
 	shutdownCh chan struct{}
-}
-
-// NewApp creates a new application instance
-func NewApp(config Config) (*App, error) {
-	conn, err := dbus.SystemBus()
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to system bus: %w", err)
-	}
-
-	app := &App{
-		config:     config,
-		dbusConn:   conn,
-		values:     make(map[int]map[objectpath]dbus.Variant),
-		shutdownCh: make(chan struct{}),
-	}
-
-	// Initialize the values maps
-	app.values[0] = make(map[objectpath]dbus.Variant) // For VALUE variant
-	app.values[1] = make(map[objectpath]dbus.Variant) // For STRING variant
-
-	return app, nil
 }
 
 type singlePhase struct {
@@ -104,6 +59,25 @@ const intro = `
       <arg direction="out" type="a{sa{sv}}" name="values"/>
     </method>
 	</interface>` + introspect.IntrospectDataString + `</node> `
+
+func generateRandomPhase() *singlePhase {
+	L := singlePhase{}
+
+	// Generate random voltage (220-240V range)
+	L.voltage = 220 + rand.Float32()*20
+
+	// Generate random power (-5000 to 5000W to simulate import/export)
+	L.power = (rand.Float32() - 0.5) * 10000
+
+	// Calculate current from power and voltage (I = P/V)
+	L.a = L.power / L.voltage
+
+	// Generate random energy values (accumulated kWh)
+	L.forward = rand.Float64() * 10000 // 0-10000 kWh imported
+	L.reverse = rand.Float64() * 10000 // 0-10000 kWh exported
+
+	return &L
+}
 
 type objectpath string
 
@@ -165,35 +139,11 @@ func init() {
 	}
 
 	log.SetLevel(ll)
-
-	isEnergyMeter = os.Getenv("SMA_ENERGY_METER") == "true"
-	if isEnergyMeter {
-		log.Printf("SMA Energy Meter, NOT SHM2!")
-	}
 }
 
-func (a *App) HandleMessage(src *net.UDPAddr, n int, b []byte) {
-
-	if n < 500 {
-		log.Debug("Received packet is probably too small. Size: ", n)
-		log.Debug("Serial: ", binary.BigEndian.Uint32(b[20:24]))
-		return
-	}
-
-	// 0-28: SMA/SUSyID/SN/Uptime
-	if a.config.SMASusyID > 0 && uint32(a.config.SMASusyID) != binary.BigEndian.Uint32(b[20:24]) {
-		log.Debugf("Oops, I was told to only listen for updates from %d, but this update is from %d", a.config.SMASusyID, binary.BigEndian.Uint32(b[20:24]))
-		return
-	}
+func (a *App) GenerateRandomData() {
 	log.Debug("----------------------")
-	log.Debug("Received datagram from meter")
-
-	// There are some broadcast packets caught by the multicast listener, that the meter is sending to 9522.
-	// See https://github.com/mitchese/shm-et340/issues/2
-	if binary.BigEndian.Uint16(b[16:18]) != 24681 {
-		log.Debug("This is a broadcast packet, not from the meter")
-		return
-	}
+	log.Debug("Generating random meter data")
 
 	changedItems := make(map[string]map[string]dbus.Variant)
 
@@ -219,34 +169,26 @@ func (a *App) HandleMessage(src *net.UDPAddr, n int, b []byte) {
 		}
 	}
 
-	powertot := ((float32(binary.BigEndian.Uint32(b[32:36])) - float32(binary.BigEndian.Uint32(b[52:56]))) / 10.0)
-	bezugtot := float64(binary.BigEndian.Uint64(b[40:48])) / 3600.0 / 1000.0
-	einsptot := float64(binary.BigEndian.Uint64(b[60:68])) / 3600.0 / 1000.0
+	// Generate random values for 3 phases
+	L1 := generateRandomPhase()
+	L2 := generateRandomPhase()
+	L3 := generateRandomPhase()
 
-	var L1, L2, L3 *singlePhase
-
-	if isEnergyMeter {
-		L1 = decodePhaseChunk(b[156:300])
-		L2 = decodePhaseChunk(b[300:444])
-		L3 = decodePhaseChunk(b[444:588])
-	} else {
-		L1 = decodePhaseChunk(b[164:308])
-		L2 = decodePhaseChunk(b[308:452])
-		L3 = decodePhaseChunk(b[452:596])
-	}
+	// Calculate totals
+	powertot := L1.power + L2.power + L3.power
+	bezugtot := L1.forward + L2.forward + L3.forward
+	einsptot := L1.reverse + L2.reverse + L3.reverse
+	totalCurrent := L1.a + L2.a + L3.a
+	totalVoltage := (L1.voltage + L2.voltage + L3.voltage) / 3.0
 
 	if log.IsLevelEnabled(log.DebugLevel) {
 		PrintPhaseTable(L1, L2, L3)
 	}
 
-	// --- Use the new helper to batch updates with correct formatting ---
-	// Using 1 decimal for power, 2 for energy/voltage/current is a safe bet.
-	// First, Totals
+	// Update totals
 	update("/Ac/Power", "W", float64(powertot), 1)
 	update("/Ac/Energy/Reverse", "kWh", einsptot, 2)
 	update("/Ac/Energy/Forward", "kWh", bezugtot, 2)
-	totalCurrent := L1.a + L2.a + L3.a
-	totalVoltage := (L1.voltage + L2.voltage + L3.voltage) / 3.0
 	update("/Ac/Current", "A", float64(totalCurrent), 2)
 	update("/Ac/Voltage", "V", float64(totalVoltage), 2)
 
@@ -274,7 +216,28 @@ func (a *App) HandleMessage(src *net.UDPAddr, n int, b []byte) {
 	// finally, post the updates
 	a.emitItemsChanged(changedItems)
 
-	log.Info(fmt.Sprintf("Meter update received and published to D-Bus: %.1f W", powertot))
+	log.Info(fmt.Sprintf("Random data published to D-Bus: %.1f W", powertot))
+}
+
+// NewApp creates a new application instance
+func NewApp(config Config) (*App, error) {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to system bus: %w", err)
+	}
+
+	app := &App{
+		config:     config,
+		dbusConn:   conn,
+		values:     make(map[int]map[objectpath]dbus.Variant),
+		shutdownCh: make(chan struct{}),
+	}
+
+	// Initialize the values maps
+	app.values[0] = make(map[objectpath]dbus.Variant) // For VALUE variant
+	app.values[1] = make(map[objectpath]dbus.Variant) // For STRING variant
+
+	return app, nil
 }
 
 // Run starts the application
@@ -287,10 +250,22 @@ func (a *App) Run() error {
 		return fmt.Errorf("failed to register DBus paths: %w", err)
 	}
 
-	log.Info("Successfully connected to dbus and registered as a meter... Commencing reading of the SMA meter")
+	log.Info("Successfully connected to dbus and registered as a meter... Starting random data generation")
 
-	// Start multicast listener
-	multicast.Listen(a.config.MulticastAddress, a.HandleMessage)
+	// Start timer to generate random data every 5 seconds
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				a.GenerateRandomData()
+			case <-a.shutdownCh:
+				return
+			}
+		}
+	}()
 
 	// Wait for shutdown signal
 	<-a.shutdownCh
@@ -305,83 +280,16 @@ func (a *App) Shutdown() {
 	}
 }
 
-func main() {
-	// Configure logging
-	lvl, ok := os.LookupEnv("LOG_LEVEL")
-	if !ok {
-		lvl = "info"
-	}
-
-	ll, err := log.ParseLevel(lvl)
-	if err != nil {
-		ll = log.DebugLevel
-	}
-	log.SetLevel(ll)
-
-	// Create configuration
-	config := Config{
-		MulticastAddress: "239.12.255.254:9522",
-		DBusName:         "com.victronenergy.grid.cgwacs_ttyUSB0_di30_mb1",
-		LogLevel:         lvl,
-	}
-
-	// Parse SMA Susy ID if provided
-	if smasusyIDStr := os.Getenv("SMASUSYID"); smasusyIDStr != "" {
-		if id, err := strconv.ParseUint(smasusyIDStr, 10, 32); err == nil {
-			config.SMASusyID = uint32(id)
-		}
-	}
-
-	// Create and run application
-	app, err := NewApp(config)
-	if err != nil {
-		log.Fatalf("Failed to create application: %v", err)
-	}
-
-	// Set the global app instance
-	SetApp(app)
-
-	// Handle shutdown signals
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-sigCh
-		log.Info("Received shutdown signal, cleaning up...")
-		app.Shutdown()
-		os.Exit(0)
-	}()
-
-	// Run the application
-	if err := app.Run(); err != nil {
-		log.Fatalf("Application error: %v", err)
-	}
-}
-
-func decodePhaseChunk(b []byte) *singlePhase {
-
-	// why does this measure in 1/10 of watts?!
-	bezugW := float32(binary.BigEndian.Uint32(b[4:8])) / 10.0
-	einspeiseW := float32(binary.BigEndian.Uint32(b[24:28])) / 10.0
-
-	// this is in watt seconds ... chagne to kilo(1000)watthour(3600)s:
-	bezugkWh := float64(binary.BigEndian.Uint64(b[12:20])) / 3600.0 / 1000.0
-	einspeisekWh := float64(binary.BigEndian.Uint64(b[32:40])) / 3600.0 / 1000.0
-
-	// not used, but leaving here for future
-	//bezugVA := float32(binary.BigEndian.Uint32(b[84:88])) / 10
-	//einspeiseVA := float32(binary.BigEndian.Uint32(b[104:108])) / 10
-
-	L := singlePhase{}
-	L.voltage = float32(binary.BigEndian.Uint32(b[132:136])) / 1000 // millivolts!
-	L.power = bezugW - einspeiseW
-	L.a = L.power / L.voltage
-	L.forward = bezugkWh
-	L.reverse = einspeisekWh
-
-	return &L
-	//log.Println(phase, "Buy: ", float32(binary.BigEndian.Uint32(b[4:8]))/10)
-	//log.Println(phase, "Sell: ", float32(binary.BigEndian.Uint32(b[24:28]))/10)
-	//return
+func PrintPhaseTable(L1, L2, L3 *singlePhase) {
+	log.Println("+-----+-------------+---------------+---------------+")
+	log.Println("|value|   L1 \t|     L2  \t|   L3  \t|")
+	log.Println("+-----+-------------+---------------+---------------+")
+	log.Printf("|  V  | %8.2f \t| %8.2f \t| %8.2f \t|", L1.voltage, L2.voltage, L3.voltage)
+	log.Printf("|  A  | %8.2f \t| %8.2f \t| %8.2f \t|", L1.a, L2.a, L3.a)
+	log.Printf("|  W  | %8.2f \t| %8.2f \t| %8.2f \t|", L1.power, L2.power, L3.power)
+	log.Printf("| kWh | %8.2f \t| %8.2f \t| %8.2f \t|", L1.forward, L2.forward, L3.forward)
+	log.Printf("| kWh | %8.2f \t| %8.2f \t| %8.2f \t|", L1.reverse, L2.reverse, L3.reverse)
+	log.Println("+-----+-------------+---------------+---------------+")
 }
 
 func (a *App) RegisterDBusPaths() error {
@@ -461,12 +369,6 @@ func (a *App) InitializeValues() {
 
 	a.values[0]["/Mgmt/ProcessVersion"] = dbus.MakeVariant("1.8.0")
 	a.values[1]["/Mgmt/ProcessVersion"] = dbus.MakeVariant("1.8.0")
-
-	// these used to be in the old demo, but have been removed. Not sure what they did, but they may be useful in the future
-	//a.values[0]["/Position"] = dbus.MakeVariantWithSignature(0, dbus.SignatureOf(123))
-	///a.values[1]["/Position"] = dbus.MakeVariant("0")
-	//a.values[0]["/ProductId"] = dbus.MakeVariant(45058)
-	//a.values[1]["/ProductId"] = dbus.MakeVariant("45058")
 
 	a.values[0]["/ProductName"] = dbus.MakeVariant("Grid meter")
 	a.values[1]["/ProductName"] = dbus.MakeVariant("Grid meter")
@@ -551,14 +453,46 @@ func (a *App) GetItems() (map[string]map[string]dbus.Variant, *dbus.Error) {
 	return items, nil
 }
 
-func PrintPhaseTable(L1, L2, L3 *singlePhase) {
-	log.Println("+-----+-------------+---------------+---------------+")
-	log.Println("|value|   L1 \t|     L2  \t|   L3  \t|")
-	log.Println("+-----+-------------+---------------+---------------+")
-	log.Printf("|  V  | %8.2f \t| %8.2f \t| %8.2f \t|", L1.voltage, L2.voltage, L3.voltage)
-	log.Printf("|  A  | %8.2f \t| %8.2f \t| %8.2f \t|", L1.a, L2.a, L3.a)
-	log.Printf("|  W  | %8.2f \t| %8.2f \t| %8.2f \t|", L1.power, L2.power, L3.power)
-	log.Printf("| kWh | %8.2f \t| %8.2f \t| %8.2f \t|", L1.forward, L2.forward, L3.forward)
-	log.Printf("| kWh | %8.2f \t| %8.2f \t| %8.2f \t|", L1.reverse, L2.reverse, L3.reverse)
-	log.Println("+-----+-------------+---------------+---------------+")
+func main() {
+	// Configure logging
+	lvl, ok := os.LookupEnv("LOG_LEVEL")
+	if !ok {
+		lvl = "info"
+	}
+
+	ll, err := log.ParseLevel(lvl)
+	if err != nil {
+		ll = log.DebugLevel
+	}
+	log.SetLevel(ll)
+
+	// Create configuration
+	config := Config{
+		DBusName: "com.victronenergy.grid.cgwacs_ttyUSB0_di30_mb1",
+		LogLevel: lvl,
+	}
+
+	// Create and run application
+	app, err := NewApp(config)
+	if err != nil {
+		log.Fatalf("Failed to create application: %v", err)
+	}
+
+	// Set the global app instance
+	SetApp(app)
+
+	// Handle shutdown signals
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigCh
+		log.Info("Received shutdown signal, cleaning up...")
+		app.Shutdown()
+		os.Exit(0)
+	}()
+
+	// Run the application
+	if err := app.Run(); err != nil {
+		log.Fatalf("Application error: %v", err)
+	}
 }
